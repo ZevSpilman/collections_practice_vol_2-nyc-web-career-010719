aad9a2e48c52de5d62925a7b5db383d65a52609e
Zev Spilman <zevspilman@yahoo.com>
2019-01-04 00:28:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
-# your code goes here
+def begins_with_r(some)
+  i = 0
+  while i < some.length
+   i += 1
+    some.each do |smtn|
+     if !smtn.start_with? "r"  
+       return false
+     
+  
+end
+end
+return true
+end
+end
+
+def contain_a(elements)
+  newArr = []
+  i = 0
+  while i < elements.length 
+  elements.each do |smtn|
+    i += 1
+    if smtn.include? "a"
+      newArr.push(smtn)
+end
+end
+return newArr
+end
+end
+
+def first_wa(elements)
+  elements.detect do |smtn|
+    smtn[0,2] == "wa"
+  end
+end
+
+def remove_non_strings(array)
+  array.delete_if do |smtn|
+    smtn.is_a?(String) == false
+  end
+end
